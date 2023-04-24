9e627e9ac4fcbb566a1fc3564066b60b80315462
AlejoEpico <alejozona@hotmail.com>
2023-04-24 18:01:10 -0500

    Add Python email script for automating invitations.

--- emailSender.py
+++ emailSender.py
@@ -1,38 +1,11 @@
-# import smtplib
-
-# # Set up the SMTP server
-# smtp_server = 'smtp.gmail.com'  # replace with your email provider's SMTP server
-# smtp_port = 587  # replace with your email provider's SMTP port
-# smtp_username = 'your_email@gmail.com'  # replace with your email address
-# smtp_password = 'your_password'  # replace with your email password
-# sender_email = 'your_email@gmail.com'  # replace with your email address
-
-# # Set up the email message
-# receiver_email = 'recipient_email@example.com'  # replace with the recipient email address
-# subject = 'Subject line of the email'
-# body = 'Body of the email'
-
-# message = f'Subject: {subject}\n\n{body}'
-
-# # Send the email message
-# try:
-#     server = smtplib.SMTP(smtp_server, smtp_port)
-#     server.starttls()
-#     server.login(smtp_username, smtp_password)
-#     server.sendmail(sender_email, receiver_email, message)
-#     print('Email sent successfully!')
-# except Exception as e:
-#     print(f'Error sending email: {str(e)}')
-# finally:
-#     server.quit()
 
 
 from email.message import EmailMessage
 import ssl
 import smtplib
 
-email_sender = 'alejoepico@gmail.com'
-email_password = "mrrmfsqklcdmjsdf"
+email_sender = ''  # Here insert your email
+email_password = ''  # Here insert your app password
 email_receiver = 'pmuw76d@femailtor.com'
 
 subject = 'Please play videogames'
